3eadbdced6780729a04057be0a3c076fbb85db6c
Aeroeia <1269938269@qq.com>
2025-05-21 09:13:00 +0800

    添加分库分表功能

--- server/program-service/src/main/java/com/example/mapper/TicketCategoryRelationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.TicketCategoryRelation;
-
-public interface TicketCategoryRelationMapper extends BaseMapper<TicketCategoryRelation> {
-
-}
