9338bfcc7bc36557baadc90ab3d0a2c599fb2fc2
Aeroeia <1269938269@qq.com>
2025-05-21 11:52:00 +0800

    添加分库分表功能

---
+++ server/user-service/src/main/java/com/example/service/UserMobileService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.entity.UserMobile;
+import com.example.mapper.UserMobileMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMobileService extends ServiceImpl<UserMobileMapper, UserMobile> {
+
+}
