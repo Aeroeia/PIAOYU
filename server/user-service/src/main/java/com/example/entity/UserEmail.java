4c902fce508ebccd842e3f87ce64a934787f15d2
Aeroeia <1269938269@qq.com>
2025-05-28 23:06:00 +0800

    用户功能补充

---
+++ server/user-service/src/main/java/com/example/entity/UserEmail.java
@@ -0,0 +1,29 @@
+package com.example.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.example.data.BaseData;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@TableName("d_user_email")
+public class UserEmail extends BaseData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键id
+     */
+    private Long id;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+}
