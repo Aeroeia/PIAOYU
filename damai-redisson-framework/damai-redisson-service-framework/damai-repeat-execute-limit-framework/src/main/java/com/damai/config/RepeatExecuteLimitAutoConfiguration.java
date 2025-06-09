c63ce641d5276f61e728ca0aedcaa1cce6cbafe4
Aeroeia <1269938269@qq.com>
2025-06-09 21:39:00 +0800

    幂等组件优化

--- damai-redisson-framework/damai-redisson-service-framework/damai-repeat-execute-limit-framework/src/main/java/com/damai/config/RepeatExecuteLimitAutoConfiguration.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-repeat-execute-limit-framework/src/main/java/com/damai/config/RepeatExecuteLimitAutoConfiguration.java
@@ -25,3 +25,4 @@ public class RepeatExecuteLimitAutoConfiguration {
         return new RepeatExecuteLimitAspect(localLockCache, lockInfoHandleFactory,serviceLockFactory,redissonDataHandle);
     }
 }
+    
