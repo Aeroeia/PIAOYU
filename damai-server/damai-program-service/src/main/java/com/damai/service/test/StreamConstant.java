a6fed4f254baf62744f38abf347cbfefee5bb875
Aeroeia <1269938269@qq.com>
2025-07-02 10:56:00 +0800

    添加订阅逻辑

--- damai-server/damai-program-service/src/main/java/com/damai/service/test/StreamConstant.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/test/StreamConstant.java
@@ -1,8 +1,8 @@
-package com.damai.service.test;
-
-public class StreamConstant {
-    
-    public static final String streamName = "test_stream";
-    
-    public static final String consumerGroup = "test_group";
-}
+//package com.damai.service.test;
+//
+////public class StreamConstant {
+//    
+//    public static final String streamName = "test_stream";
+//    
+//    public static final String consumerGroup = "test_group";
+//}
