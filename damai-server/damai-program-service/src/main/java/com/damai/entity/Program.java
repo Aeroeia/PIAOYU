0e5b9798891b5f0ff468fc5f7fddca6cb7b886b2
Aeroeia <1269938269@qq.com>
2025-07-04 13:07:00 +0800

    优化节目信息

--- damai-server/damai-program-service/src/main/java/com/damai/entity/Program.java
+++ damai-server/damai-program-service/src/main/java/com/damai/entity/Program.java
@@ -190,7 +190,7 @@ public class Program extends BaseTableData implements Serializable {
     /**
      * 电子票说明
      */
-    private Integer electronicDeliveryTicketExplain;
+    private String electronicDeliveryTicketExplain;
     
     /**
      * 电子发票 1:是 0:不是
@@ -200,7 +200,7 @@ public class Program extends BaseTableData implements Serializable {
     /**
      * 电子发票说明
      */
-    private Integer electronicInvoiceExplain;
+    private String electronicInvoiceExplain;
     
     /**
      * 高热度节目 0:否 1:是
