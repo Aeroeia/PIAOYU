0ecb565003bee65dac502e89e68492ec1d5a0ee5
Aeroeia <1269938269@qq.com>
2025-07-09 17:27:00 +0800

    优化节目的初始化行为

--- damai-server/damai-program-service/src/main/java/com/damai/service/composite/ProgramBloomFilterCheckHandler.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/composite/ProgramBloomFilterCheckHandler.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProgramDetailCheckHandler extends AbstractComposite<ProgramGetDto> {
+public class ProgramBloomFilterCheckHandler extends AbstractComposite<ProgramGetDto> {
     
     @Autowired
     private BloomFilterHandler bloomFilterHandler;
