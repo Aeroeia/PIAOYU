bb645af8551b92841b211b6ac1ed805a277666c0
Aeroeia <1269938269@qq.com>
2025-07-09 09:56:00 +0800

    修改模块

--- damai-server/damai-job-service/src/main/java/com/damai/mapper/JobInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.damai.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.damai.entity.JobInfo;
-
-public interface JobInfoMapper extends BaseMapper<JobInfo> {
-}
