3588327be990f1b817882fd50b133c2228f48d72
Aeroeia <1269938269@qq.com>
2025-07-11 16:48:00 +0800

    优化推荐列表功能

--- damai-server-client/damai-program-client/src/main/java/com/damai/dto/ProgramRecommendListDto.java
+++ damai-server-client/damai-program-client/src/main/java/com/damai/dto/ProgramRecommendListDto.java
@@ -4,17 +4,13 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 @Data
 @ApiModel(value="ProgramRecommendListDto", description ="节目推荐列表")
 public class ProgramRecommendListDto {
     
-    @ApiModelProperty(name ="areaId", dataType ="Long", value ="所在区域id",required = true)
-    @NotNull
+    @ApiModelProperty(name ="areaId", dataType ="Long", value ="所在区域id")
     private Long areaId;
     
-    @ApiModelProperty(name ="parentProgramCategoryId", dataType ="Long", value ="父节目类型id",required = true)
-    @NotNull
+    @ApiModelProperty(name ="parentProgramCategoryId", dataType ="Long", value ="父节目类型id")
     private Long parentProgramCategoryId;
 }
