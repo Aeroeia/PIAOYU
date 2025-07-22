4610710990eb74a825aa55d0ae0f68f72215142f
Aeroeia <1269938269@qq.com>
2025-07-22 21:10:00 +0800

    优化节目类型加载逻辑

--- damai-server/damai-program-service/src/main/java/com/damai/service/ProgramService.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/ProgramService.java
@@ -151,6 +151,9 @@ public class ProgramService extends ServiceImpl<ProgramMapper, Program> {
     private TicketCategoryService ticketCategoryService;
     
     @Autowired
+    private ProgramCategoryService programCategoryService;
+    
+    @Autowired
     private ProgramEs programEs;
     
     @Autowired
@@ -703,8 +706,7 @@ public class ProgramService extends ServiceImpl<ProgramMapper, Program> {
     }
     
     public ProgramCategory getProgramCategory(Long programCategoryId){
-        return redisCache.getForHash(RedisKeyBuild.createRedisKey(RedisKeyManage.PROGRAM_CATEGORY_HASH)
-                ,String.valueOf(programCategoryId),ProgramCategory.class);
+        return programCategoryService.getProgramCategory(programCategoryId);
     }
     
     @Transactional(rollbackFor = Exception.class)
--- damai-server/damai-program-service/src/main/java/com/damai/service/init/ProgramCategoryInitData.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/init/ProgramCategoryInitData.java
@@ -1,37 +1,16 @@
 package com.damai.service.init;
 
-import cn.hutool.core.collection.CollectionUtil;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.damai.BusinessThreadPool;
-import com.damai.core.RedisKeyManage;
-import com.damai.entity.ProgramCategory;
 import com.damai.initialize.base.AbstractApplicationPostConstructHandler;
-import com.damai.mapper.ProgramCategoryMapper;
-import com.damai.redis.RedisCache;
-import com.damai.redis.RedisKeyBuild;
-import com.damai.servicelock.LockType;
-import com.damai.servicelock.annotion.ServiceLock;
+import com.damai.service.ProgramCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static com.damai.core.DistributedLockConstants.PROGRAM_CATEGORY_LOCK;
 @Component
 public class ProgramCategoryInitData extends AbstractApplicationPostConstructHandler {
     
     @Autowired
-    private ProgramCategoryMapper programCategoryMapper;
-    
-    @Autowired
-    private RedisCache redisCache;
-    
-    @Autowired
-    private ProgramCategoryInitData programCategoryInitDataProxy;
+    private ProgramCategoryService programCategoryService;
     
     
     @Override
@@ -42,18 +21,7 @@ public class ProgramCategoryInitData extends AbstractApplicationPostConstructHan
     @Override
     public void executeInit(final ConfigurableApplicationContext context) {
         BusinessThreadPool.execute(() -> {
-            programCategoryInitDataProxy.programCategoryRedisDataInit();
+            programCategoryService.programCategoryRedisDataInit();
         });
-    }
-    
-    @ServiceLock(lockType= LockType.Write,name = PROGRAM_CATEGORY_LOCK,keys = {"#all"})
-    public void programCategoryRedisDataInit(){
-        QueryWrapper<ProgramCategory> lambdaQueryWrapper = Wrappers.emptyWrapper();
-        List<ProgramCategory> programCategoryList = programCategoryMapper.selectList(lambdaQueryWrapper);
-        if (CollectionUtil.isNotEmpty(programCategoryList)) {
-            Map<String, ProgramCategory> programCategoryMap = programCategoryList.stream().collect(
-                    Collectors.toMap(p -> String.valueOf(p.getId()), p -> p, (v1, v2) -> v2));
-            redisCache.putHash(RedisKeyBuild.createRedisKey(RedisKeyManage.PROGRAM_CATEGORY_HASH),programCategoryMap);
-        }
     }
 }
