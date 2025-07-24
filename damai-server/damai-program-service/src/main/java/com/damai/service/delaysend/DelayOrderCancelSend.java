ef4cefef152920bbc28fb0157ac28f16ff8ba475
Aeroeia <1269938269@qq.com>
2025-07-24 12:08:00 +0800

    修改延迟队列时间

--- damai-server/damai-program-service/src/main/java/com/damai/service/delaysend/DelayOrderCancelSend.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/delaysend/DelayOrderCancelSend.java
@@ -1,9 +1,14 @@
 package com.damai.service.delaysend;
 
 import com.damai.context.DelayQueueContext;
+import com.damai.core.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static com.damai.service.constant.ProgramOrderConstant.DELAY_ORDER_CANCEL_TIME;
+import static com.damai.service.constant.ProgramOrderConstant.DELAY_ORDER_CANCEL_TIME_UNIT;
+import static com.damai.service.constant.ProgramOrderConstant.DELAY_ORDER_CANCEL_TOPIC;
 
 @Slf4j
 @Component
@@ -13,13 +18,13 @@ public class DelayOrderCancelSend {
     private DelayQueueContext delayQueueContext;
     
     public void sendMessage(String message){
-//        try {
-//            log.info("延迟订单取消消息进行发送 消息体 : {}",message);
-//            delayQueueContext.sendMessage(SpringUtil.getPrefixDistinctionName() + "-" + DELAY_ORDER_CANCEL_TOPIC,
-//                    message, DELAY_ORDER_CANCEL_TIME, DELAY_ORDER_CANCEL_TIME_UNIT);
-//        }catch (Exception e) {
-//            log.error("send message error message : {}",message,e);
-//        }
+        try {
+            log.info("延迟订单取消消息进行发送 消息体 : {}",message);
+            delayQueueContext.sendMessage(SpringUtil.getPrefixDistinctionName() + "-" + DELAY_ORDER_CANCEL_TOPIC,
+                    message, DELAY_ORDER_CANCEL_TIME, DELAY_ORDER_CANCEL_TIME_UNIT);
+        }catch (Exception e) {
+            log.error("send message error message : {}",message,e);
+        }
         
     }
 }
