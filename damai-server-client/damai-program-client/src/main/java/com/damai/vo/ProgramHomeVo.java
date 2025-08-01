49709887d413ed859a89323a5d947813610e4c7e
Aeroeia <1269938269@qq.com>
2025-08-01 11:29:00 +0800

    优化主页查询列表

--- damai-server-client/damai-program-client/src/main/java/com/damai/vo/ProgramHomeVo.java
+++ damai-server-client/damai-program-client/src/main/java/com/damai/vo/ProgramHomeVo.java
@@ -8,13 +8,16 @@ import java.io.Serializable;
 import java.util.List;
 
 @Data
-@ApiModel(value="ProgramListVo", description ="节目主页列表")
+@ApiModel(value="ProgramHomeVo", description ="节目主页列表")
 public class ProgramHomeVo implements Serializable  {
     
     private static final long serialVersionUID = 1L;
     
     @ApiModelProperty(name ="categoryName", dataType ="String", value ="类型名字")
     private String categoryName;
+    
+    @ApiModelProperty(name ="categoryId", dataType ="Long", value ="类型id")
+    private Long categoryId;
     
     @ApiModelProperty(name ="programListVoList", dataType ="array", value ="节目列表")
     private List<ProgramListVo> programListVoList;
