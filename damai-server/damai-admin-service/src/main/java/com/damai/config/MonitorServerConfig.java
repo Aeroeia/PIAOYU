4efaea6045a7316fc5653dd4613acdf1b08c0280
Aeroeia <1269938269@qq.com>
2025-08-12 21:58:00 +0800

    升级springboot版本

--- damai-server/damai-admin-service/src/main/java/com/damai/config/MonitorServerConfig.java
+++ damai-server/damai-admin-service/src/main/java/com/damai/config/MonitorServerConfig.java
@@ -5,7 +5,9 @@ import com.damai.monitor.MonitorServer;
 import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MonitorServerConfig {
     
     @Value("${dingtalk.token:}")
--- damai-spring-cloud-framework/damai-service-gray-transition-framework/damai-service-gray-transition-base-framework/src/main/java/com/damai/balance/config/ExtraRibbonAutoConfiguration.java
+++ damai-spring-cloud-framework/damai-service-gray-transition-framework/damai-service-gray-transition-base-framework/src/main/java/com/damai/balance/config/ExtraRibbonAutoConfiguration.java
@@ -1,8 +1,6 @@
 package com.damai.balance.config;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 
-@RibbonClients(defaultConfiguration = { WorkLoadBalanceConfiguration.class })
 public class ExtraRibbonAutoConfiguration {
 }
