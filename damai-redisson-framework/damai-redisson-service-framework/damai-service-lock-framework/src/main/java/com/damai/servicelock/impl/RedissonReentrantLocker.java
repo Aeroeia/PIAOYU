0dedb638a366f63a0591e4771c0006330d29b056
Aeroeia <1269938269@qq.com>
2025-08-19 20:37:00 +0800

    修复分布式锁类型

--- damai-redisson-framework/damai-redisson-service-framework/damai-service-lock-framework/src/main/java/com/damai/servicelock/impl/RedissonReentrantLocker.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-service-lock-framework/src/main/java/com/damai/servicelock/impl/RedissonReentrantLocker.java
@@ -14,7 +14,7 @@ public class RedissonReentrantLocker implements ServiceLocker {
     
     @Override
     public RLock getLock(String lockKey) {
-        return redissonClient.getFairLock(lockKey);
+        return redissonClient.getLock(lockKey);
     }
     
     @Override
