5ef6eb972f35f4744ff0b35089670fa8d24807f1
Aeroeia <1269938269@qq.com>
2025-08-23 14:33:00 +0800

    优化用户登录逻辑

--- damai-server/damai-program-service/src/main/java/com/damai/service/TestService.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/TestService.java
@@ -1,11 +1,15 @@
 package com.damai.service;
 
 import com.baidu.fsg.uid.utils.PaddedAtomicLong;
+import com.damai.dto.TestDto;
 import com.damai.dto.TestSendDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import static com.damai.constant.Constant.TRACE_ID;
 
 @Slf4j
 @Service
@@ -17,5 +21,9 @@ public class TestService {
     public Boolean reset(final TestSendDto testSendDto) {
         count.set(0);
         return true;
+    }
+    
+    public void test(TestDto testDto, HttpServletRequest request) {
+        log.info("获取的链路id:{}",request.getHeaders(TRACE_ID));
     }
 }
