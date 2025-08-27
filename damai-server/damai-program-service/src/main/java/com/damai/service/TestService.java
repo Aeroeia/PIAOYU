316e6cfea5b65574fd8559c89b1d51a2a3f467c3
Aeroeia <1269938269@qq.com>
2025-08-28 01:14:00 +0800

    优化缓存删除逻辑

--- damai-server/damai-program-service/src/main/java/com/damai/service/TestService.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/TestService.java
@@ -1,15 +1,11 @@
 package com.damai.service;
 
 import com.baidu.fsg.uid.utils.PaddedAtomicLong;
-import com.damai.dto.TestDto;
 import com.damai.dto.TestSendDto;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
-
-import static com.damai.constant.Constant.TRACE_ID;
 
 @Slf4j
 @Service
@@ -17,13 +13,8 @@ public class TestService {
     
     AtomicLong count = new PaddedAtomicLong(0);
     
-    
     public Boolean reset(final TestSendDto testSendDto) {
         count.set(0);
         return true;
-    }
-    
-    public void test(TestDto testDto, HttpServletRequest request) {
-        log.info("获取的链路id:{}",request.getHeaders(TRACE_ID));
     }
 }
