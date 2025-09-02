b44c992557219850d81aaa4776aef2ae9cbee433
Aeroeia <1269938269@qq.com>
2025-09-02 19:12:00 +0800

    优化锁的创建行为

--- damai-redisson-framework/damai-redisson-service-framework/damai-service-lock-framework/src/main/java/com/damai/servicelock/factory/ServiceLockFactory.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-service-lock-framework/src/main/java/com/damai/servicelock/factory/ServiceLockFactory.java
@@ -1,35 +1,35 @@
 package com.damai.servicelock.factory;
 
 import com.damai.servicelock.LockType;
-import com.damai.servicelock.impl.RedissonFairLocker;
-import com.damai.servicelock.impl.RedissonReadLocker;
-import com.damai.servicelock.impl.RedissonReentrantLocker;
-import com.damai.servicelock.impl.RedissonWriteLocker;
 import com.damai.servicelock.ServiceLocker;
 import lombok.AllArgsConstructor;
-import org.redisson.api.RedissonClient;
 
 @AllArgsConstructor
 public class ServiceLockFactory {
-
     
-    private final RedissonClient redissonClient;
+    private final ServiceLocker redissonFairLocker;
+    
+    private final ServiceLocker redissonWriteLocker;
+    
+    private final ServiceLocker redissonReadLocker;
+    
+    private final ServiceLocker redissonReentrantLocker;
     
 
     public ServiceLocker getLock(LockType lockType){
         ServiceLocker lock;
         switch (lockType) {
             case Fair:
-                lock = new RedissonFairLocker(redissonClient);
+                lock = redissonFairLocker;
                 break;
             case Write:
-                lock = new RedissonWriteLocker(redissonClient);
+                lock = redissonWriteLocker;
                 break;
             case Read:
-                lock = new RedissonReadLocker(redissonClient);
+                lock = redissonReadLocker;
                 break;
             default:
-                lock = new RedissonReentrantLocker(redissonClient);
+                lock = redissonReentrantLocker;
                 break;
         }
         return lock;
